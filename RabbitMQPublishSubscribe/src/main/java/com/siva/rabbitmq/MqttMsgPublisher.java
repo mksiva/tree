d115a485916c332360ffa5ad8c4b08a9bcd57726
mksiva <pkt.siva@gmail.com>
2015-05-06 10:24:11 +0530

    Rabbit MQ samples for publishing and subscribing (AMQP and MQTT)

--- RabbitMQPublishSubscribe/src/main/java/com/siva/rabbitmq/MqttMsgPublisher.java
+++ RabbitMQPublishSubscribe/src/main/java/com/siva/rabbitmq/MqttMsgPublisher.java
@@ -12,6 +12,10 @@ import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.MqttTopic;
 
+/**
+ * 
+ * @author siva
+ */
 public class MqttMsgPublisher {
 
     MqttClient myClient;
